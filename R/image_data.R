f995750a76935c7e31a2dc5a72914f6c505be1ef
Xiyu <pengx1@mskcc.org>
2023-12-19 15:22:06 -0500

    Update image_data.R

--- R/image_data.R
+++ R/image_data.R
@@ -1,14 +1,14 @@
 #' Example input data for SpaTopic
 #'
-#' A lung cancer tumor tissue image, as the input of SpaTopic
+#' multiplexed image data on tumor tissue sample from non small cell lung cancer patient
 #'
 #' @format ## `lung5`
 #' A data frame with 100149 rows and 4 columns: 
 #' \describe{ 
-#'   \item{image}{Image ID}
-#'   \item{X}{X coordinate of the cell}
-#'   \item{Y}{Y coordinate of the cell}
-#'   \item{type}{Cell type}
+#'   \item{image} Image ID
+#'   \item{X} X coordinate of the cell
+#'   \item{Y} Y coordinate of the cell
+#'   \item{type} cell type
 #' } 
 #' @source <https://nanostring.com/products/cosmx-spatial-molecular-imager/ffpe-dataset/nsclc-ffpe-dataset/>
 #' @seealso \code{\link{SpaTopic_inference}},\code{\link{Seurat5obj_to_SpaTopic}}
