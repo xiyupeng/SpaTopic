0788c993a51c9ef49ecbfb5c85f0e4875afaeaaf
Xiyu <pengx1@mskcc.org>
2023-12-19 16:02:16 -0500

    Updates

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # SpaTopic 1.0
 
+* Initial submission to CRAN
+
 # SpaTopic 0.99
 
 * The Version under development
--- R/image_data.R
+++ R/image_data.R
@@ -5,10 +5,10 @@
 #' @format ## `lung5`
 #' A data frame with 100149 rows and 4 columns: 
 #' \describe{ 
-#'   \item{image} Image ID
-#'   \item{X} X coordinate of the cell
-#'   \item{Y} Y coordinate of the cell
-#'   \item{type} cell type
+#'   \item{image}{Image ID}
+#'   \item{X}{X coordinate of the cell}
+#'   \item{Y}{Y coordinate of the cell}
+#'   \item{type}{cell type}
 #' } 
 #' @source <https://nanostring.com/products/cosmx-spatial-molecular-imager/ffpe-dataset/nsclc-ffpe-dataset/>
 #' @seealso \code{\link{SpaTopic_inference}},\code{\link{Seurat5obj_to_SpaTopic}}
